b752f3781c49ae4a47a32523be44ccb7de8022db
pandaman64 <kointosudesuyo@infoseek.jp>
2024-08-03 19:11:19 +0900

    Update Lean and dependencies

--- lakefile.lean
+++ lakefile.lean
@@ -13,12 +13,9 @@ lean_lib RegexCorrectness where
 -- TODO: make a proper test_driver
 lean_exe Test where
 
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4" @ "v4.8.0"
-
-require Parser from git
-  "https://github.com/fgdorais/lean4-parser" @ "3d98183d1446d27c831e911be1f428f2b0466eb6"
+require "leanprover-community" / "mathlib" @ git "v4.10.0"
+require "fgdorais" / "Parser" @ git "56d6a480d666c33262a30c87a9e8b74e83805288"
 
 -- TODO: stop using meta if
 meta if get_config? doc = some "on" then -- do not download and build doc-gen4 by default
-require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "c7f4ac84b973b6efd8f24ba2b006cad1b32c9c53"
+require "leanprover" / "doc-gen4" @ git "32c52a4b35ba8a3d261056e0d264de7bb94c062e"
