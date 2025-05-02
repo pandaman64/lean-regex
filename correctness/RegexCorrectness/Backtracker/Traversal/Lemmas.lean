54f913a006b6eff6e770d8ac3ee5121397ab1f9b
pandaman64 <kointosudesuyo@infoseek.jp>
2025-05-02 23:21:00 +0900

    Prepare for using invariants

---
+++ correctness/RegexCorrectness/Backtracker/Traversal/Lemmas.lean
@@ -0,0 +1,9 @@
+import RegexCorrectness.Backtracker.Traversal.Invariants
+
+set_option autoImplicit false
+
+open Regex.Data (BitMatrix BoundedIterator)
+
+namespace Regex.Backtracker
+
+end Regex.Backtracker
