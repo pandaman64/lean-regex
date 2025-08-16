870797dfa4a09328d75619ec9e04170366ea48b5
Krishna Padmasola <kpadmasola@gmail.com>
2025-08-16 11:52:19 +0530

    Addressed review comments

--- correctness/RegexCorrectness/Display/NFADisplay.lean
+++ correctness/RegexCorrectness/Display/NFADisplay.lean
@@ -6,14 +6,15 @@ import Regex.Data.Classes
 import Regex.Syntax.Parser
 
 open Lean Meta ProofWidgets Jsx Regex.Syntax.Parser
-open scoped Jsx in
 
-def epsilon : String := "\\epsilon"
+namespace RegexCorrectness.Display.NFADisplay
 
-def isNeighbor (i j : Nat) : String :=
+private def epsilon : String := "\\epsilon"
+
+private def isNeighbor (i j : Nat) : String :=
   if i - j == 1 then "TRUE" else "FALSE"
 
-def escapeLabel (label : String) : String :=
+private def escapeLabel (label : String) : String :=
   label.replace "_" "\\_"
 
 def labelElement (elemLabel label : String) : String :=
@@ -26,22 +27,22 @@ def labelElement (elemLabel label : String) : String :=
   else
     s!"Label {elemLabel} $\\text\{{escapeLabel label}}$\n"
 
-def createNode (i : Nat) : String :=
+private def createNode (i : Nat) : String :=
   s!"Let n{i} := Node(\"{i}\")\n"
 
-def labelNode (i : Nat) (label : String) : String :=
+private def labelNode (i : Nat) (label : String) : String :=
   labelElement s!"n{i}" label
 
-def declareStartNode (i : Nat) : String :=
+private def declareStartNode (i : Nat) : String :=
   s!"StartNode(n{i})\n"
 
-def relateSuccessorNodes (i : Nat) : String :=
+private def relateSuccessorNodes (i : Nat) : String :=
   s!"Successor(n{i}, n{i - 1})\n"
 
-def declareDoneNode (i : Nat) : String :=
+private def declareDoneNode (i : Nat) : String :=
   s!"DoneNode(n{i})\n"
 
-def createTransition (i j : Nat) (label : String) : String :=
+private def createTransition (i j : Nat) (label : String) : String :=
   let transition := s!"Let t_{i}_{j} := Transition(n{i}, n{j}, {isNeighbor i j})\n"
   let transitionLabel := labelElement s!"t_{i}_{j}" label
   transition ++ transitionLabel
@@ -82,7 +83,8 @@ Let FALSE := False()
         sub := sub ++ labelNode i s!"split {next₁} {next₂}"
         sub := sub ++ createTransition i next₁ epsilon
         sub := sub ++ createTransition i next₂ epsilon
-    | .fail => ()
+    | .fail =>
+        sub := sub ++ labelNode i "fail"
     | .anchor a next =>
         sub := sub ++ labelNode i s!"anchor {next}"
         sub := sub ++ createTransition i next s!"{Anchor.toString a}"
@@ -100,17 +102,19 @@ def mkNFADiagram (regex : Regex) : MetaM Html :=
       sty={include_str "NFAPenrose.style"}
       sub={nfaToSubstance regex} />
 
+end RegexCorrectness.Display.NFADisplay
+
 -- Use this for testing purposes. Sample regexes avauilable in  regex/tests/Test.lean
 def sampleRegex : Regex :=
   --re! r##"a|"##
   --re! r##"(?:|a)+"##
-  re! r##"[a-zA-Z0-9_]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}"##
-  -- re! r##"\w+"##
+  --re! r##"[a-zA-Z0-9_]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}"##
+  --re! r##"\w+"##
   --re! r"\d{4}-\d{2}-\d{2}"
   --re! r##"^\b"##
   --re! r##"^\b$"##
   --re! r##"(?:foo|bar|[A-Z])\b"##
-  --re! r##"(?:foo|bar|[A-Z])\B"##
+  re! r##"(?:foo|bar|[A-Z])\B"##
 
 
 -- Uncomment the following lines to debug issues with the diagram
@@ -118,4 +122,5 @@ def sampleRegex : Regex :=
 -- #eval IO.println substance
 
 -- Place your cursor at the end of the following line to see the NFA diagram
+open RegexCorrectness.Display.NFADisplay
 #html mkNFADiagram sampleRegex
