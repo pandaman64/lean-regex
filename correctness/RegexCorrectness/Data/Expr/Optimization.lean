32f4e91ba62ec06b21fafd19de7476fea58015a9
pandaman64 <kointosudesuyo@infoseek.jp>
2025-12-04 20:14:40 +0900

    Migrate OptimizationInfo to ValidPos

--- correctness/RegexCorrectness/Data/Expr/Optimization.lean
+++ correctness/RegexCorrectness/Data/Expr/Optimization.lean
@@ -10,8 +10,8 @@ theorem empty_of_captures_of_nullOnly {s} {p p' : ValidPos s} {groups e} (c : Ex
   induction c <;> grind [nullOnly]
 
 open Std in
-theorem curr_of_captures_of_firstChars_some {s} {p p' : ValidPos s} {groups e n cs}
-  (c : Expr.Captures p p' groups e) (h : e.firstChars n = .some cs) (ne : p ≠ s.endValidPos) :
+theorem contains_get_of_captures_of_firstChars_some {s} {p p' : ValidPos s} {groups e n cs} {ne : p ≠ s.endValidPos}
+  (c : Expr.Captures p p' groups e) (h : e.firstChars n = .some cs) :
   cs.contains (p.get ne) := by
   revert cs
   induction c with
@@ -23,13 +23,13 @@ theorem curr_of_captures_of_firstChars_some {s} {p p' : ValidPos s} {groups e n 
     simp only [firstChars, HashSet.union_eq, Option.pure_def, Option.bind_eq_bind,
       Option.bind_eq_some_iff, Option.some.injEq] at h
     have ⟨_, hcs₁, _, _, hcs⟩ := h
-    simpa [←hcs, HashSet.mem_union_iff] using .inl (ih ne hcs₁)
+    simpa [←hcs, HashSet.mem_union_iff] using .inl (ih hcs₁)
   | alternateRight _ ih =>
     intro cs h
     simp only [firstChars, HashSet.union_eq, Option.pure_def, Option.bind_eq_bind,
       Option.bind_eq_some_iff, Option.some.injEq] at h
     have ⟨_, _, _, hcs₂, hcs⟩ := h
-    simpa [← hcs, HashSet.mem_union_iff] using .inr (ih ne hcs₂)
+    simpa [← hcs, HashSet.mem_union_iff] using .inr (ih hcs₂)
   | concat c₁ _ ih₁ ih₂ =>
     intro cs h
     simp [firstChars] at h
@@ -37,7 +37,7 @@ theorem curr_of_captures_of_firstChars_some {s} {p p' : ValidPos s} {groups e n 
     all_goals simp [Option.bind_eq_some_iff] at h
     next h' =>
       obtain rfl := empty_of_captures_of_nullOnly c₁ h'
-      exact ih₂ ne h.1
-    next => exact ih₁ ne h.1
+      exact ih₂ h.1
+    next => exact ih₁ h.1
 
 end Regex.Data.Expr
--- correctness/RegexCorrectness/Regex/OptimizationInfo.lean
+++ correctness/RegexCorrectness/Regex/OptimizationInfo.lean
@@ -4,41 +4,31 @@ import RegexCorrectness.Data.Expr.Optimization
 import RegexCorrectness.Data.String
 
 open Regex.Data (Expr CaptureGroups)
-open String (Iterator)
+open String (ValidPos)
 open Std
 
 namespace Regex.OptimizationInfo
 
-@[simp, grind =]
-theorem findStart_toString {it : Iterator} {opt : OptimizationInfo} : (opt.findStart it).toString = it.toString := by
-  unfold findStart
-  grind
+theorem findStart_le_pos {s : String} {pos : ValidPos s} {opt : OptimizationInfo} : pos ≤ opt.findStart pos := by
+  fun_cases findStart opt pos
+  next => exact ValidPos.find_le_pos
+  next => exact ValidPos.le_refl _
 
-theorem findStart_le_pos {it : Iterator} {opt : OptimizationInfo} : it.pos ≤ (opt.findStart it).pos := by
-  fun_cases findStart opt it
-  next => exact Iterator.find_le_pos
-  next => exact Nat.le_refl _
-
-theorem valid_findStart_of_valid {it : Iterator} {opt : OptimizationInfo} (v : it.Valid) : (opt.findStart it).Valid := by
-  fun_cases findStart opt it
-  next => exact Iterator.find_valid_of_valid v
-  next => exact v
-
-theorem findStart_completeness {it it' it'' : Iterator} {opt : OptimizationInfo} {groups : CaptureGroups} {e : Expr}
-  (eq : opt = .fromExpr e) (v : it.Valid) (eqs : it'.toString = it.toString) (ge : it.pos ≤ it'.pos)
-  (lt : it'.pos < (opt.findStart it).pos) (c : e.Captures it' it'' groups) :
+theorem findStart_completeness {s : String} {pos pos' pos'' : ValidPos s} {opt : OptimizationInfo} {groups : CaptureGroups s} {e : Expr}
+  (eq : opt = .fromExpr e) (ge : pos ≤ pos') (lt : pos' < opt.findStart pos) (c : e.Captures pos' pos'' groups) :
   False := by
   revert lt
-  fun_cases findStart opt it
+  fun_cases findStart opt pos
   next cs h =>
     intro lt
-    have : ¬(cs.contains it'.curr) := Iterator.find_completeness v it' c.validL eqs ge lt
-    simp [eq, fromExpr] at h
+    have ne : pos' ≠ s.endValidPos := ValidPos.ne_endValidPos_of_lt lt
+    have : ¬(cs.contains (pos'.get ne)) := ValidPos.find_completeness ge lt
+    simp only [eq, fromExpr, Option.map_eq_some_iff] at h
     have ⟨_, h₁, h₂⟩ := h
-    have : cs.contains it'.curr := by
+    have : cs.contains (pos'.get ne) := by
       rw [← h₂]
       simp only [HashSet.contains_toArray]
-      exact Expr.curr_of_captures_of_firstChars_some c h₁
+      exact Expr.contains_get_of_captures_of_firstChars_some c h₁
     contradiction
   next => exact Nat.not_lt_of_ge ge
 
